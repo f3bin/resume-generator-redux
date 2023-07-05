e482d812a3d94b07d97c295c672793973f95eee7
Febin <febinj14@gmail.com>
2023-07-05 17:39:26 +0530

    ui

--- src/components/form2/Form2.jsx
+++ src/components/form2/Form2.jsx
@@ -3,7 +3,7 @@ import { UseSelector, useSelector } from "react-redux";
 import "./Form2.scss";
 
 const Form2 = () => {
-  const name = useSelector(state =>state.form.name)
+  const name = useSelector((state) => state.form.name);
   return (
     <div className="form2-container">
       <h3>Tell more about you !</h3>
@@ -15,13 +15,33 @@ const Form2 = () => {
           <input type="number" />
           <label>city</label>
           <input type="text" />
+        </div>
+        <div className="form2-aboutYou-right">
+          <input className="icon-fileUpload" type="file" name="profile" />
           <label>phone</label>
           <input type="number" />
           <label>email</label>
           <input type="email" />
         </div>
-        <div className="form2-aboutYou-right">
-          <input className="icon-fileUpload" type="file" name="profile" />
+      </div>
+
+      <h3>Other details</h3>
+      <div className="otherDetails">
+        <div className="otherDetails-left">
+          <h3>Educational Details</h3>
+          <label>Institute </label>
+          <input type="text" />
+          <label>Course </label>
+          <input type="text" />
+        </div>
+        <div className="otherDetails-right">
+          <h3>Experiences</h3>
+          <label>Job title</label>
+          <input type="text" />
+          <label>Employer</label>
+          <input type="text" />
+          <label>Duration(years)</label>
+          <input type="number" />
         </div>
       </div>
     </div>
